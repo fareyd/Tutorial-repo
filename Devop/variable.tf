588c4d4c3a84b7da49aa58a1f0c205354be08f13
fareyd <fareydinix@gmail.com>
2023-12-22 21:49:06 +0000

    variable.tf

---
+++ Devop/variable.tf
@@ -0,0 +1,24 @@
+variable "cidr_block" {
+    type = list(string)
+    default = ["172.20.0.0/16","172.20.10.0/24"]
+}
+
+variable "ports" {
+    type = list(number)
+    default = [22,80,443,8080,8081,9000]
+}
+
+variable "ami" {
+    type = string
+    default = "ami-05fb0b8c1424f266b"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "instance_type_for_nexus" {
+    type = string
+    default = "t2.medium"
+}
